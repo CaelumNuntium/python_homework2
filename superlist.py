65e3b65eccae576bc8da3fd904205c37dc02b560
CaelumNuntium <st087042@student.spbu.ru>
2022-10-20 00:09:51 +0300

    Add file superlist.py

---
+++ superlist.py
@@ -0,0 +1,19 @@
+class SuperList(list):
+    def save(self, filename):
+        with open(filename, 'w') as out:
+            for i in self:
+                out.write(f"{i}\n")
+
+    @classmethod
+    def load(cls, filename):
+        res = SuperList()
+        with open(filename, 'r') as inp:
+            for s in inp:
+                res.append(float(s))
+        return res
+
+
+sl = SuperList([1, 2, 3, 4, 5, 6, 7, 8, 9, 10])
+sl.save("superlist.txt")
+s2 = SuperList.load("superlist.txt")
+print(s2)
